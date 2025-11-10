8b991c0c8b8a990d1fef2862603c0d8c523c0c3e
uladzimir23 <49497624+uladzimir23@users.noreply.github.com>
2025-11-10 18:55:51 +0300

    mm

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 // src/app/main.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { App } from '@/App';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root container not found');
